269dc6dc6479791be1be86ed67a0294cd4ae188b
PragmaticLinux <info@pragmaticlinux.com>
2020-12-16 22:13:23 +0100

    Corrected setuptools related files after testing.

---
+++ yasymon/__init__.py
@@ -0,0 +1,2 @@
+# Import the entry point into the application.
+from yasymon.app import main
--- yasymon/__main__.py
+++ yasymon/__main__.py
@@ -1,5 +1,6 @@
-from app import main
+# Import the application to access the main() entry point.
+import app
 
 # Start the application by calling its entry point
-main()
+app.main()
 
